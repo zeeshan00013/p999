6a8bbc01a16a7e7ca5cee1481d3c3003e6ee3504
zeeshan00013 <za953042@gmail.com>
2025-09-13 14:14:14 +0500

    done

--- app/robots.txt/route.js
+++ app/robots.txt/route.js
@@ -2,7 +2,7 @@ export async function GET() {
   const body = `User-agent: *
 Allow: /
 
-Sitemap: https://www.w567game.cc/sitemap.xml
+Sitemap: https://www.w567game.cc/sitemap.xml/
 `;
 
   return new Response(body, {
